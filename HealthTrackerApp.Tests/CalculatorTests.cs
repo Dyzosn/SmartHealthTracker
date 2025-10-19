bf472bac8a6bd991ef75af1ea91569177b34772a
indomielover86 <felixsonwongso2016@gmail.com>
2025-10-20 07:17:30 +1100

    Add NUnit test project and verify test framework setup

---
+++ HealthTrackerApp.Tests/CalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace HealthTrackerApp.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void TestSetup_Works()
+        {
+            // Verify NUnit test framework is properly configured
+            Assert.Pass("NUnit setup successful!");
+        }
+    }
+}
