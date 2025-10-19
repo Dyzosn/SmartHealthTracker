d8168fa3f68fa8c7894b4d9abb6798503ce09ca1
stnly999 <stnly999@gmail.com>
2025-10-19 21:38:36 +1100

    Add ReportsAnalyticsForm with statistical analysis

---
+++ HealthTrackerApp/Forms/ReportsAnalyticsForm.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace HealthTrackerApp.Forms
+{
+    partial class ReportsAnalyticsForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblStartDate = new System.Windows.Forms.Label();
+            this.dtpStartDate = new System.Windows.Forms.DateTimePicker();
+            this.lblEndDate = new System.Windows.Forms.Label();
+            this.dtpEndDate = new System.Windows.Forms.DateTimePicker();
+            this.btnGenerateReport = new System.Windows.Forms.Button();
+            this.lblSummaryStats = new System.Windows.Forms.Label();
+            this.lblDailyBreakdown = new System.Windows.Forms.Label();
+            this.dgvDailyBreakdown = new System.Windows.Forms.DataGridView();
+            this.lblMealTypeAnalysis = new System.Windows.Forms.Label();
+            this.dgvMealTypeAnalysis = new System.Windows.Forms.DataGridView();
+            this.lblTopFoods = new System.Windows.Forms.Label();
+            this.dgvTopFoods = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDailyBreakdown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMealTypeAnalysis)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTopFoods)).BeginInit();
+            this.SuspendLayout();
+
+            // lblTitle
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(20, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(230, 30);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Reports & Analytics";
+
+            // lblStartDate
+            this.lblStartDate.AutoSize = true;
+            this.lblStartDate.Location = new System.Drawing.Point(20, 70);
+            this.lblStartDate.Name = "lblStartDate";
+            this.lblStartDate.Size = new System.Drawing.Size(65, 15);
+            this.lblStartDate.TabIndex = 1;
+            this.lblStartDate.Text = "Start Date:";
+
+            // dtpStartDate
+            this.dtpStartDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpStartDate.Location = new System.Drawing.Point(90, 67);
+            this.dtpStartDate.Name = "dtpStartDate";
+            this.dtpStartDate.Size = new System.Drawing.Size(120, 23);
+            this.dtpStartDate.TabIndex = 2;
+
+            // lblEndDate
+            this.lblEndDate.AutoSize = true;
+            this.lblEndDate.Location = new System.Drawing.Point(230, 70);
+            this.lblEndDate.Name = "lblEndDate";
+            this.lblEndDate.Size = new System.Drawing.Size(58, 15);
+            this.lblEndDate.TabIndex = 3;
+            this.lblEndDate.Text = "End Date:";
+
+            // dtpEndDate
+            this.dtpEndDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpEndDate.Location = new System.Drawing.Point(295, 67);
+            this.dtpEndDate.Name = "dtpEndDate";
+            this.dtpEndDate.Size = new System.Drawing.Size(120, 23);
+            this.dtpEndDate.TabIndex = 4;
+
+            // btnGenerateReport
+            this.btnGenerateReport.Location = new System.Drawing.Point(430, 65);
+            this.btnGenerateReport.Name = "btnGenerateReport";
+            this.btnGenerateReport.Size = new System.Drawing.Size(120, 27);
+            this.btnGenerateReport.TabIndex = 5;
+            this.btnGenerateReport.Text = "Generate Report";
+            this.btnGenerateReport.UseVisualStyleBackColor = true;
+
+            // lblSummaryStats
+            this.lblSummaryStats.AutoSize = true;
+            this.lblSummaryStats.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblSummaryStats.Location = new System.Drawing.Point(20, 110);
+            this.lblSummaryStats.Name = "lblSummaryStats";
+            this.lblSummaryStats.Size = new System.Drawing.Size(200, 19);
+            this.lblSummaryStats.TabIndex = 6;
+            this.lblSummaryStats.Text = "Summary Statistics";
+
+            // lblDailyBreakdown
+            this.lblDailyBreakdown.AutoSize = true;
+            this.lblDailyBreakdown.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblDailyBreakdown.Location = new System.Drawing.Point(20, 250);
+            this.lblDailyBreakdown.Name = "lblDailyBreakdown";
+            this.lblDailyBreakdown.Size = new System.Drawing.Size(130, 20);
+            this.lblDailyBreakdown.TabIndex = 7;
+            this.lblDailyBreakdown.Text = "Daily Breakdown";
+
+            // dgvDailyBreakdown
+            this.dgvDailyBreakdown.AllowUserToAddRows = false;
+            this.dgvDailyBreakdown.AllowUserToDeleteRows = false;
+            this.dgvDailyBreakdown.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDailyBreakdown.Location = new System.Drawing.Point(20, 280);
+            this.dgvDailyBreakdown.Name = "dgvDailyBreakdown";
+            this.dgvDailyBreakdown.ReadOnly = true;
+            this.dgvDailyBreakdown.Size = new System.Drawing.Size(530, 150);
+            this.dgvDailyBreakdown.TabIndex = 8;
+
+            // lblMealTypeAnalysis
+            this.lblMealTypeAnalysis.AutoSize = true;
+            this.lblMealTypeAnalysis.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblMealTypeAnalysis.Location = new System.Drawing.Point(570, 250);
+            this.lblMealTypeAnalysis.Name = "lblMealTypeAnalysis";
+            this.lblMealTypeAnalysis.Size = new System.Drawing.Size(155, 20);
+            this.lblMealTypeAnalysis.TabIndex = 9;
+            this.lblMealTypeAnalysis.Text = "Meal Type Analysis";
+
+            // dgvMealTypeAnalysis
+            this.dgvMealTypeAnalysis.AllowUserToAddRows = false;
+            this.dgvMealTypeAnalysis.AllowUserToDeleteRows = false;
+            this.dgvMealTypeAnalysis.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMealTypeAnalysis.Location = new System.Drawing.Point(570, 280);
+            this.dgvMealTypeAnalysis.Name = "dgvMealTypeAnalysis";
+            this.dgvMealTypeAnalysis.ReadOnly = true;
+            this.dgvMealTypeAnalysis.Size = new System.Drawing.Size(380, 150);
+            this.dgvMealTypeAnalysis.TabIndex = 10;
+
+            // lblTopFoods
+            this.lblTopFoods.AutoSize = true;
+            this.lblTopFoods.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblTopFoods.Location = new System.Drawing.Point(20, 450);
+            this.lblTopFoods.Name = "lblTopFoods";
+            this.lblTopFoods.Size = new System.Drawing.Size(180, 20);
+            this.lblTopFoods.TabIndex = 11;
+            this.lblTopFoods.Text = "Top 10 Foods Logged";
+
+            // dgvTopFoods
+            this.dgvTopFoods.AllowUserToAddRows = false;
+            this.dgvTopFoods.AllowUserToDeleteRows = false;
+            this.dgvTopFoods.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTopFoods.Location = new System.Drawing.Point(20, 480);
+            this.dgvTopFoods.Name = "dgvTopFoods";
+            this.dgvTopFoods.ReadOnly = true;
+            this.dgvTopFoods.Size = new System.Drawing.Size(530, 150);
+            this.dgvTopFoods.TabIndex = 12;
+
+            // ReportsAnalyticsForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 660);
+            this.Controls.Add(this.dgvTopFoods);
+            this.Controls.Add(this.lblTopFoods);
+            this.Controls.Add(this.dgvMealTypeAnalysis);
+            this.Controls.Add(this.lblMealTypeAnalysis);
+            this.Controls.Add(this.dgvDailyBreakdown);
+            this.Controls.Add(this.lblDailyBreakdown);
+            this.Controls.Add(this.lblSummaryStats);
+            this.Controls.Add(this.btnGenerateReport);
+            this.Controls.Add(this.dtpEndDate);
+            this.Controls.Add(this.lblEndDate);
+            this.Controls.Add(this.dtpStartDate);
+            this.Controls.Add(this.lblStartDate);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "ReportsAnalyticsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reports & Analytics - Health Tracker";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDailyBreakdown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMealTypeAnalysis)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTopFoods)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblStartDate;
+        private System.Windows.Forms.DateTimePicker dtpStartDate;
+        private System.Windows.Forms.Label lblEndDate;
+        private System.Windows.Forms.DateTimePicker dtpEndDate;
+        private System.Windows.Forms.Button btnGenerateReport;
+        private System.Windows.Forms.Label lblSummaryStats;
+        private System.Windows.Forms.Label lblDailyBreakdown;
+        private System.Windows.Forms.DataGridView dgvDailyBreakdown;
+        private System.Windows.Forms.Label lblMealTypeAnalysis;
+        private System.Windows.Forms.DataGridView dgvMealTypeAnalysis;
+        private System.Windows.Forms.Label lblTopFoods;
+        private System.Windows.Forms.DataGridView dgvTopFoods;
+    }
+}
---
+++ HealthTrackerApp/Forms/ReportsAnalyticsForm.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using HealthTrackerApp.Data;
+using HealthTrackerApp.Models;
+using HealthTrackerApp.Services;
+
+namespace HealthTrackerApp.Forms
+{
+    // Reports and analytics form for statistical analysis of nutrition data
+    // Displays meal summaries, trends, and top foods using DataGridView
+    public partial class ReportsAnalyticsForm : Form
+    {
+        private readonly HealthTrackerContext _context;
+        private readonly MealService _mealService;
+        private readonly User _currentUser;
+
+        public ReportsAnalyticsForm(User user)
+        {
+            InitializeComponent();
+            _currentUser = user;
+            _context = new HealthTrackerContext();
+            _mealService = new MealService(_context);
+
+            this.Load += ReportsAnalyticsForm_Load;
+        }
+
+        private void ReportsAnalyticsForm_Load(object sender, EventArgs e)
+        {
+            // Set default date range to last 30 days
+            dtpStartDate.Value = DateTime.Today.AddDays(-30);
+            dtpEndDate.Value = DateTime.Today;
+
+            btnGenerateReport.Click += btnGenerateReport_Click;
+
+            GenerateReport();
+        }
+
+        // Generate statistical report for selected date range
+        private void btnGenerateReport_Click(object sender, EventArgs e)
+        {
+            GenerateReport();
+        }
+
+        private void GenerateReport()
+        {
+            try
+            {
+                DateTime startDate = dtpStartDate.Value.Date;
+                DateTime endDate = dtpEndDate.Value.Date;
+
+                if (startDate > endDate)
+                {
+                    MessageBox.Show("Start date must be before end date", "Validation Error");
+                    return;
+                }
+
+                // Get meals for date range
+                var meals = _mealService.GetMealsByDateRange(_currentUser.UserId, startDate, endDate);
+
+                if (meals.Count == 0)
+                {
+                    MessageBox.Show("No meals found for selected date range", "No Data");
+                    return;
+                }
+
+                // Calculate summary statistics using LINQ
+                int totalMeals = meals.Count;
+                double totalCalories = meals.Sum(m => m.TotalCalories);
+                double avgCalories = meals.Average(m => m.TotalCalories);
+                double totalProtein = meals.Sum(m => m.Protein);
+                double totalCarbs = meals.Sum(m => m.Carbs);
+                double totalFats = meals.Sum(m => m.Fats);
+
+                int dayCount = (endDate - startDate).Days + 1;
+                double avgCaloriesPerDay = totalCalories / dayCount;
+
+                // Display summary statistics
+                lblSummaryStats.Text = $"Period: {startDate:dd/MM/yyyy} to {endDate:dd/MM/yyyy}\n" +
+                                      $"Total Meals: {totalMeals}\n" +
+                                      $"Total Calories: {totalCalories:F0} kcal\n" +
+                                      $"Avg Calories/Day: {avgCaloriesPerDay:F0} kcal\n" +
+                                      $"Avg Calories/Meal: {avgCalories:F0} kcal\n" +
+                                      $"Total Protein: {totalProtein:F1}g\n" +
+                                      $"Total Carbs: {totalCarbs:F1}g\n" +
+                                      $"Total Fats: {totalFats:F1}g";
+
+                // Daily breakdown analysis
+                var dailyStats = meals
+                    .GroupBy(m => m.MealDate.Date)
+                    .Select(g => new
+                    {
+                        Date = g.Key,
+                        MealCount = g.Count(),
+                        TotalCalories = g.Sum(m => m.TotalCalories),
+                        AvgCalories = g.Average(m => m.TotalCalories)
+                    })
+                    .OrderBy(x => x.Date)
+                    .ToList();
+
+                // Populate daily breakdown DataGridView
+                dgvDailyBreakdown.Rows.Clear();
+                if (dgvDailyBreakdown.Columns.Count == 0)
+                {
+                    dgvDailyBreakdown.Columns.Add("Date", "Date");
+                    dgvDailyBreakdown.Columns.Add("Meals", "Meals");
+                    dgvDailyBreakdown.Columns.Add("TotalCal", "Total Calories");
+                    dgvDailyBreakdown.Columns.Add("AvgCal", "Avg Calories");
+                }
+
+                foreach (var day in dailyStats)
+                {
+                    dgvDailyBreakdown.Rows.Add(
+                        day.Date.ToString("dd/MM/yyyy"),
+                        day.MealCount,
+                        $"{day.TotalCalories:F0}",
+                        $"{day.AvgCalories:F0}"
+                    );
+                }
+
+                // Meal type analysis
+                var mealTypeStats = meals
+                    .GroupBy(m => m.MealType)
+                    .Select(g => new
+                    {
+                        MealType = g.Key.ToString(),
+                        Count = g.Count(),
+                        AvgCalories = g.Average(m => m.TotalCalories),
+                        AvgProtein = g.Average(m => m.Protein)
+                    })
+                    .OrderByDescending(x => x.Count)
+                    .ToList();
+
+                // Populate meal type analysis DataGridView
+                dgvMealTypeAnalysis.Rows.Clear();
+                if (dgvMealTypeAnalysis.Columns.Count == 0)
+                {
+                    dgvMealTypeAnalysis.Columns.Add("Type", "Meal Type");
+                    dgvMealTypeAnalysis.Columns.Add("Count", "Count");
+                    dgvMealTypeAnalysis.Columns.Add("AvgCal", "Avg Calories");
+                    dgvMealTypeAnalysis.Columns.Add("AvgProtein", "Avg Protein");
+                }
+
+                foreach (var type in mealTypeStats)
+                {
+                    dgvMealTypeAnalysis.Rows.Add(
+                        type.MealType,
+                        type.Count,
+                        $"{type.AvgCalories:F0}",
+                        $"{type.AvgProtein:F1}g"
+                    );
+                }
+
+                // Top foods analysis
+                var mealFoods = meals
+                    .SelectMany(m => _context.MealFoods.Where(mf => mf.MealId == m.MealId))
+                    .ToList();
+
+                var topFoods = mealFoods
+                    .GroupBy(mf => mf.FoodId)
+                    .Select(g => new
+                    {
+                        FoodId = g.Key,
+                        TimesLogged = g.Count(),
+                        TotalCalories = g.Sum(mf => mf.Calories)
+                    })
+                    .OrderByDescending(x => x.TimesLogged)
+                    .Take(10)
+                    .ToList();
+
+                // Populate top foods DataGridView
+                dgvTopFoods.Rows.Clear();
+                if (dgvTopFoods.Columns.Count == 0)
+                {
+                    dgvTopFoods.Columns.Add("Food", "Food Name");
+                    dgvTopFoods.Columns.Add("Times", "Times Logged");
+                    dgvTopFoods.Columns.Add("Calories", "Total Calories");
+                }
+
+                foreach (var food in topFoods)
+                {
+                    var foodEntity = _context.Foods.Find(food.FoodId);
+                    dgvTopFoods.Rows.Add(
+                        foodEntity.FoodName,
+                        food.TimesLogged,
+                        $"{food.TotalCalories:F0}"
+                    );
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error generating report: {ex.Message}", "Error");
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            _context?.Dispose();
+        }
+    }
+}
